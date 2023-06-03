ce0d49bf745c37bd8d0855918a5db8b4b9a0312e
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 19:14:32 +0100

    Added Buttons to Landing

--- darkmode.js
+++ darkmode.js
@@ -3,10 +3,14 @@ onStart();
 function toggleDark() {
     var body = document.body;
     const projects = document.getElementsByClassName("project");
+    const left_projects = document.getElementsByClassName("double-project-panel-left");
+    const right_projects = document.getElementsByClassName("double-project-panel-right");
     body.classList.toggle("darkmode");
     
     for(let i = 0; i < projects.length; i++){
         projects[i].classList.toggle("project_darkmode");
+        left_projects[i].classList.toggle("project_darkmode");
+        right_projects[i].classList.toggle("project_darkmode");
     }
     if(body.classList.contains("darkmode")){
         localStorage.setItem('darkMode', 'enabled');
@@ -18,12 +22,16 @@ function toggleDark() {
 function onStart(){
   var body = document.body;
   const projects = document.getElementsByClassName("project");
+  const left_projects = document.getElementsByClassName("double-project-panel-left");
+  const right_projects = document.getElementsByClassName("double-project-panel-right");
   if (localStorage.getItem('darkMode') == 'enabled') {
     body.classList.add('darkmode');
     const checkbox = document.getElementById('darkmode');
     checkbox.checked = true;
     for(let i = 0; i < projects.length; i++){
         projects[i].classList.toggle("project_darkmode");
+        left_projects[i].classList.toggle("project_darkmode");
+        right_projects[i].classList.toggle("project_darkmode");
     }
   }
 }
